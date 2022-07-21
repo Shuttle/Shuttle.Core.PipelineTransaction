4692c1b3c2907f19bd2bcfe395d0f28c7bf6cb4a
Eben <me@ebenroux.co.za>
2022-07-21 13:34:30 +0200

    - options

---
+++ Shuttle.Core.PipelineTransaction/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shuttle.Core.Contract;
+using Shuttle.Core.Pipelines;
+
+namespace Shuttle.Core.PipelineTransaction
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPipelineTransaction(this IServiceCollection services)
+        {
+            Guard.AgainstNull(services, nameof(services));
+
+            services.AddPipelineObservers(typeof(TransactionScopeObserver).Assembly);
+
+            return services;
+        }
+    }
+}
--- Shuttle.Core.PipelineTransaction/TransactionScopeObserver.cs
+++ Shuttle.Core.PipelineTransaction/TransactionScopeObserver.cs
@@ -19,15 +19,16 @@ namespace Shuttle.Core.PipelineTransaction
     public class TransactionScopeObserver : ITransactionScopeObserver
     {
         private readonly ITransactionScopeFactory _transactionScopeFactory;
-        private readonly TransactionScopeSettings _settings;
+        private readonly TransactionScopeOptions _transactionScopeOptions;
 
-        public TransactionScopeObserver(ITransactionScopeFactory transactionScopeFactory, IOptions<TransactionScopeSettings> options)
+        public TransactionScopeObserver(IOptions<TransactionScopeOptions> transactionScopeOptions, ITransactionScopeFactory transactionScopeFactory)
         {
+            Guard.AgainstNull(transactionScopeOptions, nameof(transactionScopeOptions));
+            Guard.AgainstNull(transactionScopeOptions.Value, nameof(transactionScopeOptions.Value));
             Guard.AgainstNull(transactionScopeFactory, nameof(transactionScopeFactory));
-            Guard.AgainstNull(options, nameof(options));
 
+            _transactionScopeOptions = transactionScopeOptions.Value;
             _transactionScopeFactory = transactionScopeFactory;
-            _settings = options.Value;
         }
 
         public void Execute(OnAbortPipeline pipelineEvent)
@@ -113,7 +114,7 @@ namespace Shuttle.Core.PipelineTransaction
                         MethodBase.GetCurrentMethod()?.Name ?? Resources.MethodNameNotFound));
             }
 
-            scope = _transactionScopeFactory.Create(_settings.IsolationLevel, _settings.Timeout);
+            scope = _transactionScopeFactory.Create(_transactionScopeOptions.IsolationLevel, _transactionScopeOptions.Timeout);
 
             state.SetTransactionScope(scope);
         }
