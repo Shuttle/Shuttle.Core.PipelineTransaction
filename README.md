f1ddbe429b2db2095847f6490f12bde2d5b69054
Eben <me@ebenroux.co.za>
2022-08-07 15:00:58 +0200

    - readme / builder

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Shuttle.Core.PipelineTransaction
 
 Provides a pipeline observer to handle transaction scopes.
+
+```c#
+services.AddPipelineProcessing(builder => {
+    builder.AddAssembly(assembly);
+    builder.AddTransactions();
+});
+```
+
---
+++ Shuttle.Core.PipelineTransaction/PipelineProcessingBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Shuttle.Core.Contract;
+using Shuttle.Core.Pipelines;
+
+namespace Shuttle.Core.PipelineTransaction
+{
+    public static class PipelineProcessingBuilderExtensions
+    {
+        public static PipelineProcessingBuilder AddTransactions(this PipelineProcessingBuilder builder)
+        {
+            Guard.AgainstNull(builder, nameof(builder));
+
+            builder.AddAssembly(typeof(TransactionScopeObserver).Assembly);
+
+            return builder;
+        }
+    }
+}
